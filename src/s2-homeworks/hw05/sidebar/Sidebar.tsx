506ad11a89d8a9019ad1cdf7eac3224c9c93cdd9
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2022-11-21 00:54:33 +0300

    add styles

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -33,7 +33,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={({isActive}) => isActive ? s.active : s.notActive}  // делает студент
+                        className={({isActive}) => isActive ? s.active : ''}  // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -42,7 +42,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={({isActive}) => isActive ? s.active : s.notActive}  // делает студент
+                        className={({isActive}) => isActive ? s.active : ''}  // делает студент
                     >
                         Junior
                     </NavLink>
@@ -51,7 +51,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={({isActive}) => isActive ? s.active : s.notActive}  // делает студент
+                        className={({isActive}) => isActive ? s.active : ''}  // делает студент
                     >
                         Junior Plus
                     </NavLink>
